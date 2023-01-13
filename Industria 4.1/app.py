efba0bac6cf82cfae96763005847a17c770b850b
GRJR1325 <97265804+GRJR1325@users.noreply.github.com>
2023-01-13 12:44:32 -0600

    Update app.py

--- Industria 4.1/app.py
+++ Industria 4.1/app.py
@@ -14,7 +14,10 @@ class Index:
 
 class Bienvenida:
     def GET(self):
-        return render.bienvenida()
-
+        num1 = 10
+        num2 = 15
+        suma = num1 + num2
+        return render.bienvenida(suma)
+        #return render.bienvenida("Si funciona")
 if __name__ == "__main__":
     app.run()
