079d2f9806b723d7c0461aa6d0a026498c7c6d0a
GRJR1325 <97265804+GRJR1325@users.noreply.github.com>
2023-01-26 07:30:15 -0600

    Add files via upload

---
+++ Industria 4.1/1. Iniciar Sesion/app.py
@@ -0,0 +1,23 @@
+import web
+
+urls = (
+    '/', 'formulario' 
+)
+
+app = web.application(urls, globals())
+render = web.template.render("templates/")
+
+class Formulario:
+    def GET(self):
+        return render.formulario()
+
+    def POST(self):
+        form = web.input()
+        print(type(form))
+        return form["nombre"]
+    
+if __name__ == "__main__":
+    web.config.debug= False
+    app.run()
+
+    
